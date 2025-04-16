862807c751c33914c69b87e16f4429cad381ae4b
adikri <20814110+adikri@users.noreply.github.com>
2025-04-16 17:51:41 +0530

    Title: 살 수 있는 물건, Time: 189ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250416/살 수 있는 물건/things-able-to-buy.js
@@ -0,0 +1,10 @@
+let fs = require("fs");
+let money = Number(fs.readFileSync(0).toString().trim());
+
+if(money >= 3000){
+    console.log("book");
+}else if(money >= 1000){
+    console.log("mask");
+}else{
+    console.log("no");
+}
