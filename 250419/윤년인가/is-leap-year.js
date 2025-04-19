affc2417f0d4846b24d5fc9324bfe04be26ef36f
adikri <20814110+adikri@users.noreply.github.com>
2025-04-19 11:25:49 +0530

    Title: 윤년인가, Time: 172ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250419/윤년인가/is-leap-year.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+
+let year = Number(fs.readFileSync(0).toString().trim());
+
+if(year % 4 === 0){
+    if(year % 100 === 0 && year % 400 !== 0){
+        console.log(false);
+    }else{
+        console.log(true);
+    }
+}else{
+    console.log(false);
+}
