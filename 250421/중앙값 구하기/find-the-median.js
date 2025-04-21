28f29bbafb60c7d223ae531835cdd28dd0c89e81
adikri <20814110+adikri@users.noreply.github.com>
2025-04-21 06:35:11 +0530

    Title: 중앙값 구하기, Time: 175ms, Memory: 15MB, Status: Passed - Codetree

--- 250421/중앙값 구하기/find-the-median.js
+++ 250421/중앙값 구하기/find-the-median.js
@@ -6,10 +6,20 @@ let num1 = Number(input[0]);
 let num2 = Number(input[1]);
 let num3 = Number(input[2]);
 
-if(num1 > num2 && num1 < num3){
-    console.log(num1)
-}else if(num2 > num1 && num2 < num3){
-    console.log(num2);
-}else if(num3 > num1 && num3 < num2){
-    console.log(num3);
+if(num1 > num2 ){
+    if(num3<num2){
+        console.log(num2);
+    }else if(num3<num1){
+        console.log(num3);
+    }else{
+        console.log(num1);
+    }
+}else{
+    if(num3<num1){
+        console.log(num1);
+    }else if(num3<num2){
+        console.log(num3);
+    }else{
+        console.log(num2);
+    }
 }
