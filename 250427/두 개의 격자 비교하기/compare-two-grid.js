707ef59bb2005c1b34855847ed34eb91f8071b91
adikri <20814110+adikri@users.noreply.github.com>
2025-04-27 10:57:49 +0530

    Title: 두 개의 격자 비교하기, Time: 185ms, Memory: 14MB, Status: Runtime Error - Codetree

--- 250427/두 개의 격자 비교하기/compare-two-grid.js
+++ 250427/두 개의 격자 비교하기/compare-two-grid.js
@@ -2,23 +2,26 @@ let fs = require("fs");
 
 let input = fs.readFileSync(0).toString().trim().split("\n");
 
-let arr1 = Array(4).fill(0).map(()=>Array(4).fill(0));
-let arr2 = Array(4).fill(0).map(()=>Array(4).fill(0));
-let arr3 = Array(4).fill(0).map(()=>Array(4).fill(0));
+let n = Number(input[0].split(" ")[0]);
+let m = Number(input[0].split(" ")[1]);
 
-for(let i = 0; i < 4; i++){
+let arr1 = Array(n).fill(0).map(()=>Array(m).fill(0));
+let arr2 = Array(n).fill(0).map(()=>Array(m).fill(0));
+let arr3 = Array(n).fill(0).map(()=>Array(m).fill(0));
+
+for(let i = 0; i < n; i++){
     arr1[i] = input[i+1].split(" ").map(Number);
 }
 
-for(let i = 0; i < 4; i++){
+for(let i = 0; i < n; i++){
     arr2[i] = input[i+5].split(" ").map(Number);
 }
 
 // console.log(arr1,arr2);
 
-for(let i = 0; i < 4; i++){
+for(let i = 0; i < n; i++){
     let str = "";
-    for(let j = 0; j < 4; j++){
+    for(let j = 0; j < m; j++){
         // console.log(arr1[i][j],arr2[i][j]);
         
         if(arr1[i][j] !== arr2[i][j]){
