e32626b71a7a5d2e94faccf2fea7a4864910c5eb
adikri <20814110+adikri@users.noreply.github.com>
2025-05-07 17:49:28 +0530

    Title: 상하로 반복하며 출력, Time: 180ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250507/상하로 반복하며 출력/print-in-up-down-reverse.js
@@ -0,0 +1,15 @@
+// Variable declaration and input
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+// Repeat and print vertically.
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j < n; j++) {
+        if (j % 2 === 0) str += i + 1;
+        else str += n - i;
+    }
+    console.log(str);
+}
